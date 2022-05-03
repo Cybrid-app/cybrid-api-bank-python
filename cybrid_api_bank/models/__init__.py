78d26376a8454d9a9015e88d2150a4242ead91a2
brent-cybrid <brent@cybrid.app>
2022-05-03 12:47:32 +0000

    v0.3.17

---
+++ cybrid_api_bank/models/__init__.py
@@ -0,0 +1,43 @@
+# flake8: noqa
+
+# import all models into this package
+# if you have many models here with many references from one model to another this may
+# raise a RecursionError
+# to avoid this, import only the models that you directly need like:
+# from from cybrid_api_bank.model.pet import Pet
+# or import this package, but before doing it, use:
+# import sys
+# sys.setrecursionlimit(n)
+
+from cybrid_api_bank.model.account import Account
+from cybrid_api_bank.model.account_list import AccountList
+from cybrid_api_bank.model.asset import Asset
+from cybrid_api_bank.model.asset_list import AssetList
+from cybrid_api_bank.model.attestation_details import AttestationDetails
+from cybrid_api_bank.model.bank import Bank
+from cybrid_api_bank.model.bank_list import BankList
+from cybrid_api_bank.model.customer import Customer
+from cybrid_api_bank.model.customer_list import CustomerList
+from cybrid_api_bank.model.exchange import Exchange
+from cybrid_api_bank.model.exchange_account import ExchangeAccount
+from cybrid_api_bank.model.identity_record import IdentityRecord
+from cybrid_api_bank.model.list_request_page import ListRequestPage
+from cybrid_api_bank.model.list_request_per_page import ListRequestPerPage
+from cybrid_api_bank.model.post_account import PostAccount
+from cybrid_api_bank.model.post_bank import PostBank
+from cybrid_api_bank.model.post_customer import PostCustomer
+from cybrid_api_bank.model.post_identity_record import PostIdentityRecord
+from cybrid_api_bank.model.post_identity_record_attestation_details import PostIdentityRecordAttestationDetails
+from cybrid_api_bank.model.post_quote import PostQuote
+from cybrid_api_bank.model.post_trade import PostTrade
+from cybrid_api_bank.model.post_verification_key import PostVerificationKey
+from cybrid_api_bank.model.quote import Quote
+from cybrid_api_bank.model.quote_list import QuoteList
+from cybrid_api_bank.model.symbol_price import SymbolPrice
+from cybrid_api_bank.model.symbol_price_response import SymbolPriceResponse
+from cybrid_api_bank.model.symbols import Symbols
+from cybrid_api_bank.model.system_account import SystemAccount
+from cybrid_api_bank.model.trade import Trade
+from cybrid_api_bank.model.trade_list import TradeList
+from cybrid_api_bank.model.verification_key import VerificationKey
+from cybrid_api_bank.model.verification_key_list import VerificationKeyList
---
+++ docs/ListRequestPage.md
@@ -0,0 +1,12 @@
+# ListRequestPage
+
+The page index to retrieve.
+
+## Properties
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**value** | **int** | The page index to retrieve. | defaults to 0
+
+[[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
+
+
